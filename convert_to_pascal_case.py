bbce5f91ec870a1c4a21185a1244dcdacacda651
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-20 21:42:10 +0800

    Update convert_to_pascal_case.py
    Ask the user to input their fullname.

--- convert_to_pascal_case.py
+++ convert_to_pascal_case.py
@@ -1,4 +1,1 @@
-# Pseudocode
-# Ask the user to input their fullname
-# Convert it to PascalCase (capitalize each word and remove spaces)
-# Print the PascalCase formatted fullname
+fullname = input("Enter your fullname: ")
