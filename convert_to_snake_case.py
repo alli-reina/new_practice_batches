4642132219d2958f25412efcf89794bd3d0db045
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-20 21:59:19 +0800

    Update convert_to_snake_case.py
    Ask the user to input their fullname.

--- convert_to_snake_case.py
+++ convert_to_snake_case.py
@@ -1,4 +1,1 @@
-# Pseudocode
-# Ask the user to input their fullname.
-# Convert it to lowercase and replace spaces with underscores.
-# Print the snake_case formatted fullname.
+fullname = input("Enter your fullname: ")
