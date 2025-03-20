5320172d0076810967d0c40339035b5d69519ef2
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-20 18:35:22 +0800

    Update trim_leading_space.py
    Ask the user to input their full name

--- trim_leading_space.py
+++ trim_leading_space.py
@@ -1,4 +1,1 @@
-# Pseudocode
-# 1. Ask the user to input their fullname with leading spaces.
-# 2. Remove leading spaces from the input.
-# 3. Print the modified fullname.
+print(input("Enter your fullname: "))
