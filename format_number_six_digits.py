a08586d3c93e437d878f28f90922a423950f1fe6
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-20 18:40:50 +0800

    Update format_number_six_digits.py
    Ask the user to input a number (0-1000).

--- format_number_six_digits.py
+++ format_number_six_digits.py
@@ -1,4 +1,1 @@
-# Pseudocode
-# Ask the user to input a number (0-1000)
-# Convert the number to a 6-digit format with leading zeros
-# Print the formatted number
+ print(input('Enter a number (0-1000): '))
